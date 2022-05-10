1847ae2d634a7e87757dff0a6b9dd9c160e0caf7
chandrakala756971 <105033922+chandrakala756971@users.noreply.github.com>
2022-05-10 10:14:48 +0530

    Java Programs

---
+++ Compare.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Compare{
+public static void main(String[] args){
+System.out.println("enter a number:");
+Scanner scn=new Scanner(System.in);
+int a=scn.nextInt();
+int b=scn.nextInt();
+if(a==b){
+System.out.println("both are same");
+}
+}
+}
