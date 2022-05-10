559ecb58023dc61df5b8a50f77859b8d2c6d413b
chandrakala756971 <105033922+chandrakala756971@users.noreply.github.com>
2022-05-10 10:12:12 +0530

    Java Programs

---
+++ Product.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Product{
+public static void main(String[] args){
+Scanner scn=new Scanner(System.in);
+int a=scn.NextInt();
+int b=scn.NextInt();
+int c=a*b;
+System.out.println(c);
+}
+}
