548141c2b00e30056affffb2845568f8d4d6e134
chandrakala756971 <105033922+chandrakala756971@users.noreply.github.com>
2022-06-02 11:33:06 +0530

    MultiThreadding

---
+++ MultiThreadding/MultiThreaddingDemo.java
@@ -0,0 +1,21 @@
+package MultiThreadding;
+class Child extends Thread {
+	@Override
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
+	}
+}
+
+public class MultiThreaddingDemo {
+
+	public static void main(String[] args) {
+		Child ch = new Child();
+		ch.start();
+		for (int i = 1; i < 10; i++) {
+			System.out.println("Main Thread is Running.");
+		}
+
+}
+}
