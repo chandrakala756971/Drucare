344722bb2997ebf56159e1dc06538269aea3d659
chandrakala756971 <105033922+chandrakala756971@users.noreply.github.com>
2022-06-09 15:38:09 +0530

    repository

---
+++ Repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.Model.Products;
+
+
+public interface ProductRepository extends CrudRepository<Products,String> {
+
+}
