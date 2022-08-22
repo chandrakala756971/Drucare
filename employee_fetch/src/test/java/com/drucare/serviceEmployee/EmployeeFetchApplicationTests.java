9112c2868fddb646460bc893ca45c6cff28d7ba7
chandrakala756971 <105033922+chandrakala756971@users.noreply.github.com>
2022-08-22 18:49:57 +0530

    employeeFetch

---
+++ employee_fetch/src/test/java/com/drucare/serviceEmployee/EmployeeFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drucare.serviceEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeFetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
